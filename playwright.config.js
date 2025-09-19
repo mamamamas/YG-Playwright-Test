0aa18078943932dd41de3c43fdaf0ad366364ec0
marky <markchristiandurana75@gmail.com>
2025-09-19 21:05:51 +0800

    Updated CreateEvents

--- playwright.config.js
+++ playwright.config.js
@@ -15,13 +15,13 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : undefined, // Adjust number of workers (4 for CI)
   // workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
---
+++ tests/Account Settings/FeatureRequest.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+dotenv.config();
+test('Feature Request', async ({ page }) => {
+    await page.goto(process.env.API_URL);
+    await page.locator('.offcanvas-backdrop').click();
+    await page.getByRole('listitem').filter({ hasText: 'Loading...Account' }).getByRole('button').click();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByRole('link', { name: 'Feature Request' }).click();
+    const page1 = await page1Promise;
+    await expect(page1.locator('h1')).toContainText('Feature requests');
+});
+
+
