4068ce4e05be451929f07eb393b8bfcdd3357b08
marky <markchristiandurana75@gmail.com>
2025-09-23 17:18:02 +0800

    fix changelog

--- tests/Account Settings/ChangeLog.spec.js
+++ tests/Account Settings/ChangeLog.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 // import dotenv from 'dotenv';
 // dotenv.config();
-test('Change Log', async ({ page }) => {
+test('ChangeLog', async ({ page }) => {
     await page.goto("https://staging-v2.yearglance.com/");
     await page.locator('.offcanvas-backdrop').click();
     await page.getByRole('listitem').filter({ hasText: 'Loading...Account' }).getByRole('button').click();
