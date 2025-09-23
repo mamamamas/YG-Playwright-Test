7014e02a1bc997b16fdb8a3d1b0c547e7424e32b
marky <markchristiandurana75@gmail.com>
2025-09-23 16:08:56 +0800

    REMOVE ENV

--- playwright.config.js
+++ playwright.config.js
@@ -24,7 +24,11 @@ export default defineConfig({
   // workers: process.env.CI ? 4 : undefined, // Adjust number of workers (4 for CI)
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['json', { outputFile: 'results.json' }]
+  ],
   timeout: 45_000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
